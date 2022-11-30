04093d62ee6e09de18dc7778c83ff73198ad619f
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-30 22:44:25 +0000

    event, arrow function, alert

---
+++ dom_events.js
@@ -0,0 +1,13 @@
+// window.addEventListener("load", ()=>{
+//     console.log("window loaded");
+// });
+
+// window.onload = ()=>{
+//     console.log("window onloaded");
+// }
+
+let myButton = document.getElementById("button");
+
+myButton.addEventListener("click", ()=>{
+    alert("my button in clicked!");
+});
