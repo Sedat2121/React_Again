fe9400d5f19907bd5b9ccd4fd8d8cfce3a0764f3
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-01 00:45:27 +0000

    char limit challenge

--- dom_events.js
+++ dom_events.js
@@ -22,3 +22,36 @@ mail.addEventListener("mouseout", ()=>{
     mail.innerText = "Email Address";
     mail.style.color = "black";
 });
+
+// code challenge:  Bubbling => property transfers from a child to its parents
+
+const randomNumber = () => Math.round(Math.random() * 255);
+const randomColor = () => `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+
+// child
+let password = document.getElementById("pass");
+
+password.addEventListener("click", ()=> {
+    password.style.backgroundColor = randomColor();
+});
+
+// Parent
+password.parentElement.addEventListener("click", ()=> {
+    password.parentElement.style.backgroundColor = randomColor();
+});
+
+// grandParent
+password.parentElement.parentElement.addEventListener("click", ()=> {
+    password.parentElement.parentElement.style.backgroundColor = randomColor();
+});
+
+
+
+// character limitation challenge:
+
+let comm = document.getElementById("comment");
+let charCount = document.getElementById("charCount");
+comm.addEventListener("keyup", ()=> {
+    charCount.innerText = comm.value.length + " / 40";
+});
+
