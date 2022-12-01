f983b5ae529cab3eb6a934c03921e38057663828
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-01 12:00:44 +0000

    query Selector

---
+++ JavaScript/w3sch.js
@@ -0,0 +1,9 @@
+// Finding HTML Elements by CSS Selectors
+
+// const x = document.querySelectorAll("p.intro");
+// document.getElementById("demo").innerHTML = 
+// 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
+
+
+// Finding HTML Elements by HTML Object Collections
+
