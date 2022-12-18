d71ca55371ec0bc99a0dbcdcc8e31798981c1ac6
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-18 22:16:15 +0000

    Async setTimeout

--- JavaScript/Async.js
+++ JavaScript/Async.js
@@ -1,1 +1,13 @@
-// Asyncrom Programming 
+// Asyncronous Programming 
+console.log ('» index.js is running');
+
+let id = setTimeout( () =>  {
+    console. log('» async programming is amazing');
+}, 2000);
+console.log(id);
+clearTimeout(id); // cancel the previous timeout function
+
+let id2 = setTimeout(() => {
+console. log ('»22 async programming is amazing');
+}, 2000);
+console. log (id2);
