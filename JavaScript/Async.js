26d56ca0302a0c50f1b82cecf30fefd1db549f4d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-18 22:46:54 +0000

    setTimeout example

--- JavaScript/Async.js
+++ JavaScript/Async.js
@@ -1,13 +1,36 @@
 // Asyncronous Programming 
-console.log ('» index.js is running');
+// console.log ('» index.js is running');
 
-let id = setTimeout( () =>  {
-    console. log('» async programming is amazing');
-}, 2000);
-console.log(id);
-clearTimeout(id); // cancel the previous timeout function
+// let id = setTimeout( () =>  {
+//     console. log('» async programming is amazing');
+// }, 2000);
+// console.log(id);
+// clearTimeout(id); // cancel the previous timeout function
 
-let id2 = setTimeout(() => {
-console. log ('»22 async programming is amazing');
-}, 2000);
-console. log (id2);
+// let id2 = setTimeout(() => {
+// console. log ('»22 async programming is amazing');
+// }, 2000);
+// console. log (id2);
+
+// Example ----------------------------------------------------------------
+const slowTask = () => {
+    let i = 0;
+    let result = { firstName: 'John', lastName: 'Doe' };
+    do {
+        i++;       // some Long operation
+    } while (i < 2_000_000_000);
+    return result;
+};
+
+console.log ('» ...1'); 
+console.log ('» ...2'); 
+
+setTimeout(() => {
+    let res = slowTask();
+    console. log (res);
+});
+
+console.log('» ...3');
+console. log("» ...4");
+// ------------------------------------------------------------------------------
+
