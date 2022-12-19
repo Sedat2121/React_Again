8082f7a3c3f29e385b0b74ddf0c953b46a8f590d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-19 07:14:44 +0000

    setTimeout example

--- JavaScript/Async.js
+++ JavaScript/Async.js
@@ -13,24 +13,45 @@
 // console. log (id2);
 
 // Example ----------------------------------------------------------------
-const slowTask = () => {
-    let i = 0;
-    let result = { firstName: 'John', lastName: 'Doe' };
-    do {
-        i++;       // some Long operation
-    } while (i < 2_000_000_000);
-    return result;
-};
+// const slowTask = () => {
+//     let i = 0;
+//     let result = { firstName: 'John', lastName: 'Doe' };
+//     do {
+//         i++;       // some Long operation
+//     } while (i < 2_000_000_000);
+//     return result;
+// };
 
-console.log ('» ...1'); 
-console.log ('» ...2'); 
+// console.log ('» ...1'); 
+// console.log ('» ...2'); 
 
-setTimeout(() => {
-    let res = slowTask();
-    console. log (res);
-});
+// setTimeout(() => {
+//     let res = slowTask();
+//     console. log (res);
+// });
 
-console.log('» ...3');
-console. log("» ...4");
+// console.log('» ...3');
+// console. log("» ...4");
 // ------------------------------------------------------------------------------
+
+// Console Time Methods
+// console.time(">> >>  value: ");
+// console.log("Hello");
+// console.timeLog(">> >> new value: ");
+
+
+// setTimeout Example -----------------------------------------------------
+setTimeout(() => {
+    console.log("John: Hi");
+    setTimeout(() => {
+        console.log("Sarah: Hello");
+        setTimeout(() => {
+            console.log("John: How are you doing?");
+            setTimeout(() => {
+                console.log("Sarah: Fine, What about you?");
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000); 
+// --------------------------------------------------------------------------
 
