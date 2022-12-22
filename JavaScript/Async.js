1cf2178e3b5fb6ead763d9ce8b802849a8905f15
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-22 16:38:50 +0000

    fetch async function

--- JavaScript/Async.js
+++ JavaScript/Async.js
@@ -186,5 +186,14 @@ fetch( 'https: //restcountries.com/V3.1/name/Turkey')
     })
     .then ((data) => { 
         console. log (data);
-        console. log (data[0] capital, data[0].name. common);
+        console. log (data[0].capital, data[0].name. common);
     });
+
+const getCountry = async (countryName) => {
+    const response = await fetch(`https: //restcountries.com/V3.1/name/${countryName}`);
+    const data = await response.json();
+    console.log(data);
+    console.log(data[0].capital, data[0].name.common);
+};
+
+getCountry('Turkey');
