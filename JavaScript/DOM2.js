759fb139f4d4228096d56553129cbc8099ba4e04
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-23 23:47:06 +0000

    DOM examples

--- JavaScript/DOM2.js
+++ JavaScript/DOM2.js
@@ -1,8 +1,27 @@
 // getElement... methods -> these methods belong to document object of the DOM
-    documentgetElementById() -
-    getElementsByClassName() -
-    getElementsByName() -
-    getElementsByTagName() -
+
+documentgetElementById() -
+getElementsByClassName() -
+getElementsByName() -
+getElementsByTagName() -
+
 // querySelect... methods -> hese methods belong to Element object of the DOM
-    querySelector() -
-    querySelectorAll();
+
+querySelector() -
+querySelectorAll();
+
+
+let element = parentNode.querySelector(selector);
+
+let elementList = parentNode.querySelectorAll(selector);
+
+let nodeList = Array.from(document.querySelectorAll(selector));
+
+let parent = oneNodeInPage.parentNode;
+
+let nextSibling = currentNode.nextElementSibling;
+
+let content = document.getElementById('menu');
+let firstChild = content.firstChild.nodeName;
+
+console.log(firstChild);
