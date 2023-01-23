da370249722f79b7e30a19b96211a71169afbea7
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-23 23:42:38 +0000

    basic DOM Selectors

---
+++ JavaScript/DOM2.js
@@ -0,0 +1,8 @@
+// getElement... methods -> these methods belong to document object of the DOM
+    documentgetElementById() -
+    getElementsByClassName() -
+    getElementsByName() -
+    getElementsByTagName() -
+// querySelect... methods -> hese methods belong to Element object of the DOM
+    querySelector() -
+    querySelectorAll();
