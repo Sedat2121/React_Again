d7f8c4256693debe5cc2fe0f9e7f8c256ece2db1
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-26 23:38:02 +0000

    all combination function completed

--- JavaScript/w3sch.js
+++ JavaScript/w3sch.js
@@ -48,3 +48,28 @@ isPalindrome("");
 isPalindrome("none");
 isPalindrome("true");
 isPalindrome("nurses run");
+
+// 3. Write a JavaScript function that generates all combinations of a string
+function substrings(str1) {
+    const array1 = [];
+    for(let x=0, y=1; x<str1.length; x++, y++) {
+        array1[x] = str1.substring(x, y);
+    }
+    const combi = [];
+    let temp = "";
+    const slent = Math.pow(2, array1.length);
+
+    for(let i=0; i<slent; i++) {
+        temp = "";
+        for(let j=0; j<array1.length; j++) {
+            if(i&Math.pow(2, j)) {
+                temp += array1[j];
+            }
+        }
+        if(temp !== ""){
+            combi.push(temp);
+        }
+    }
+    console.log(combi.join("\n"));
+}
+substrings("dog");
