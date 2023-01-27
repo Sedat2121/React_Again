7c8805bfbcd93b87ba9f4ced176e3c461772390a
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-27 01:08:03 +0000

    find the longest string function completed

--- JavaScript/w3sch.js
+++ JavaScript/w3sch.js
@@ -92,4 +92,16 @@ function upper(str) {
 }
 console.log(upper("the quick brown fox"));
 
-// 
+// 6. Write a JavaScript function that accepts a string as a parameter and find the longest word within the string
+function longest(str) {
+    let array = str.match(/\w[a-z]{0,}/gi);
+    let result = array[0];
+
+    for(let x=1; x<array.length; x++){
+        if(result.length < array[x].length){
+            result = array[x];
+        }
+    }
+    return result;
+}
+console.log(longest("front end web developer"));
