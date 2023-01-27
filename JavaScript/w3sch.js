7c8611b92c54ac2917d223548bb2ad47cc9388c4
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-27 01:29:48 +0000

    vovel count function is completed

--- JavaScript/w3sch.js
+++ JavaScript/w3sch.js
@@ -1,10 +1,3 @@
-// Finding HTML Elements by CSS Selectors
-
-// const x = document.querySelectorAll("p.intro");
-// document.getElementById("demo").innerHTML = 
-// 'The first paragraph (index 0) with class="intro" is: ' + x[0].innerHTML;
-
-
 // JavaScript functions
 
 // 1. Write a JavaScript function that reverse a number
@@ -17,17 +10,17 @@ console.log(reverse(123789));
 // 2. Write a JavaScript function that checks whether a passed string is palindrome or not?
 function isPalindrome(str_entry) {
     // Change the string into lower case and remove  all non-alphanumeric characters
-    const cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+    const cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '');
     let ccount = 0;
-    if(cstr === ""){
+    if (cstr === "") {
         console.log("Nothing found!")
         return false;
     }
     // Check if the length of the string is even or odd 
-    if((cstr.length) % 2 === 0) {
+    if ((cstr.length) % 2 === 0) {
         ccount = (cstr.length) / 2;
     } else {
-        if(cstr.length === 1){
+        if (cstr.length === 1) {
             console.log("Entry ia a palindrome");
             return true;
         } else {
@@ -35,8 +28,8 @@ function isPalindrome(str_entry) {
         }
     }
     // Loop through to check the first character to the last character and then move next
-    for(let x = 0; x < ccount; x++) {
-        if(cstr[x] != cstr.slice(-1-x)[0]) {
+    for (let x = 0; x < ccount; x++) {
+        if (cstr[x] != cstr.slice(-1 - x)[0]) {
             console.log("Entry is not a palindrome");
             return false;
         }
@@ -52,21 +45,21 @@ isPalindrome("nurses run");
 // 3. Write a JavaScript function that generates all combinations of a string
 function substrings(str1) {
     const array1 = [];
-    for(let x=0, y=1; x<str1.length; x++, y++) {
+    for (let x = 0, y = 1; x < str1.length; x++, y++) {
         array1[x] = str1.substring(x, y);
     }
     const combi = [];
     let temp = "";
     const slent = Math.pow(2, array1.length);
 
-    for(let i=0; i<slent; i++) {
+    for (let i = 0; i < slent; i++) {
         temp = "";
-        for(let j=0; j<array1.length; j++) {
-            if(i&Math.pow(2, j)) {
+        for (let j = 0; j < array1.length; j++) {
+            if (i & Math.pow(2, j)) {
                 temp += array1[j];
             }
         }
-        if(temp !== ""){
+        if (temp !== "") {
             combi.push(temp);
         }
     }
@@ -85,8 +78,8 @@ function upper(str) {
     let array = str.split(' ');
     let newarray = [];
 
-    for(let i = 0; i < array.length; i++) {
-        newarray.push(array[i].charAt(0).toUpperCase()+array[i].slice(1));
+    for (let i = 0; i < array.length; i++) {
+        newarray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1));
     }
     return newarray.join(' ');
 }
@@ -97,11 +90,25 @@ function longest(str) {
     let array = str.match(/\w[a-z]{0,}/gi);
     let result = array[0];
 
-    for(let x=1; x<array.length; x++){
-        if(result.length < array[x].length){
+    for (let x = 1; x < array.length; x++) {
+        if (result.length < array[x].length) {
             result = array[x];
         }
     }
     return result;
 }
 console.log(longest("front end web developer"));
+
+// 7. Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string
+function vowel_count(str1) {
+    const vowel_list = 'aeiouAEIOU';
+    let vcount = 0;
+
+    for (let x = 0; x < str1.length; x++) {
+        if (vowel_list.indexOf(str1[x]) !== -1) {
+            vcount += 1;
+        }
+    }
+    return vcount;
+}
+console.log(vowel_count("The quick brown fox"));
