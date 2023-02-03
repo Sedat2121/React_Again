5e13c12620c4d7cc1b4ded39cac5be78e1d2fd53
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-03 23:46:55 +0000

    exponential function

--- JavaScript/w3sch.js
+++ JavaScript/w3sch.js
@@ -242,3 +242,16 @@ function amountTocoins(amount, coins) {
 } 
 console.log(amountTocoins(86, [25, 10, 5, 2,1]));
 
+// 15. Write a JavaScript function to compute the value of bn where n is the exponent and b is the bases. Accept b and n from the user and display the result
+function exp(b,n)
+{
+        var ans = 1;
+        for (var i =1; i <= n; i++)
+        {
+                ans = b * ans;        
+        }
+        return ans;
+}
+console.log(exp(2, 3));
+
+// 
