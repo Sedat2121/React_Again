59ecb1c784157d09abb85196371e1eddfbbf3515
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-17 00:33:07 +0000

    add all up

---
+++ JS_Challenges/Edabit_easy.js
@@ -0,0 +1,24 @@
+// 1. Create a function that takes a number as an argument. Add up all 
+// the numbers from 1 to the number you passed to the function.
+
+// solution1
+/*
+function addUp(num) {
+	return (num*(num+1)/2);
+}
+console.log(addUp(4));
+console.log(addUp(600));
+*/
+// solution2
+/*
+function addUp(num) {
+	let result = 0;
+    for(let i =0; i<= num; i++) {
+        result = result + i;
+    }
+    return result;
+}
+console.log(addUp(5));
+console.log(addUp(600));
+*/
+
