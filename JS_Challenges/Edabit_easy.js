5e7dbaf62953051142ff257a9830f8de621b1ff0
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-17 01:35:32 +0000

    array min, max

--- JS_Challenges/Edabit_easy.js
+++ JS_Challenges/Edabit_easy.js
@@ -4,7 +4,7 @@
 // solution1
 /*
 function addUp(num) {
-	return (num*(num+1)/2);
+    return (num*(num+1)/2);
 }
 console.log(addUp(4));
 console.log(addUp(600));
@@ -12,7 +12,7 @@ console.log(addUp(600));
 // solution2
 /*
 function addUp(num) {
-	let result = 0;
+    let result = 0;
     for(let i =0; i<= num; i++) {
         result = result + i;
     }
@@ -26,7 +26,7 @@ console.log(addUp(600));
 // and returns the number of matchsticks in that step.
 /*
 function matchHouses(step) {
-	return ((step*6)-(step-1));
+    return ((step*6)-(step-1));
 }
 
 console.log(matchHouses(33));
@@ -37,10 +37,61 @@ console.log(matchHouses(33));
 /*
 
 function shiftToLeft(x, y) {
-	// return (x * (Math.pow(2, y)));
+    // return (x * (Math.pow(2, y)));
     return (x * (2 ** y));
 }
 
 console.log(shiftToLeft(10, 3));
 console.log(shiftToLeft(-128, 3));
 */
+
+// 3. Create a function that takes an array of numbers 
+// and return both the minimum and maximum numbers, in that order.
+// solution1
+/*
+function minMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    for(i=0; i<arr.length; i++) {
+        if(min > arr[i]){
+            min = arr[i];
+        }
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+    return [min, max];
+}
+
+console.log(minMax([1, 5, 7, 9, 13])); */
+
+//Solution2
+/*
+function getMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    for (let curr of arr) {
+        if (curr > max) {
+            max = curr;
+        }
+        if (curr < min) {
+            min = curr;
+        }
+    }
+    return {min, max};
+}
+console.log(getMinMax([1, 5, 9, 34]));
+*/
+
+// Solution3
+/*
+function minMax(arr) {
+    const sortedArray = arr.sort((a, b) => a - b);
+    return {
+        min: sortedArray[0],
+        max: sortedArray[sortedArray.length - 1]
+    }
+}
+
+console.log(minMax([33, 56, 78, 1])); */
+
