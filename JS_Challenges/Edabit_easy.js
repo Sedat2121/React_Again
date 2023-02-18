1e3d6df60693e041a3a707b5d25e3b5bd54116c1
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-18 09:47:06 +0000

    Class Person

--- JS_Challenges/Edabit_easy.js
+++ JS_Challenges/Edabit_easy.js
@@ -213,3 +213,20 @@ console.log(toBinary(0xAA));
 console.log(toBinary(0xFA)); */
 
 
+/* 12. Create a method in the Person class which returns how another 
+person's age compares. Given the instances p1, p2 and p3, which will 
+be initialised with the attributes name and age, return a sentence
+in the following format:
+{other person name} is {older than / younger than / the same age as} me.
+*/
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+		
+		
+	}
+}
