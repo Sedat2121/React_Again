428b8f16453dbe4683f1e2e2232cc0febfff9d69
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-23 23:51:02 +0000

    teamwork tactics

--- JS_Challenges/Edabit-medium.js
+++ JS_Challenges/Edabit-medium.js
@@ -11,3 +11,19 @@
 
 
 // 2. Write a function redundant that takes in a string str and returns a function that returns str.
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Create a function that takes your position a and your friend's 
+// position b and returns a boolean representation of whether it's 
+// possible to earn a bonus on any die roll.
+
