0a3ae5af9273b60c86e4a4ecc73667a63ed1ff19
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-24 00:36:50 +0000

    array true functoin

--- JS_Challenges/Edabit-medium.js
+++ JS_Challenges/Edabit-medium.js
@@ -1,5 +1,17 @@
 // 1. Create a function which returns the number of true values there are in an array.
+function countTrue(arr) {
+    let counter = 0;
+	for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == true) {
+            counter++;
+        }
+        if(arr.length = 0)
+            return 0;
+    }
+    return counter;
+}
 
+console.log(countTrue([true, false, true, false]));
 
 
 
