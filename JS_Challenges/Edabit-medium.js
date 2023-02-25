62ec14d1594002ca1386a2c0eae9d1d4dd739300
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-25 23:26:37 +0000

    math derivative function

--- JS_Challenges/Edabit-medium.js
+++ JS_Challenges/Edabit-medium.js
@@ -61,3 +61,15 @@ console.log(possibleBonus(3, 5));
 console.log(possibleBonus(1, 9));
 
 console.log(possibleBonus(1, 7)); */
+
+
+// 5. Create a function that takes numbers b and m as arguments and 
+//returns the derivative of the function f(x)=x^b with respect
+// to x evaluated at x=m, where b and m are constants.
+function derivative(b, m) {
+	return b * Math.pow(m, b-1);
+}
+
+console.log(derivative(1, 4));
+console.log(derivative(3, -2));
+console.log(derivative(4, -3));
