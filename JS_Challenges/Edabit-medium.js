320fcc2fb406f5d333221e2de35b7700f72e7793
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-02-25 10:38:04 +0000

    tures in array

--- JS_Challenges/Edabit-medium.js
+++ JS_Challenges/Edabit-medium.js
@@ -1,18 +1,18 @@
 // 1. Create a function which returns the number of true values there are in an array.
 function countTrue(arr) {
     let counter = 0;
-	for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == true) {
-            counter++;
-        }
-        if(arr.length = 0)
+    if(arr.length === 0)
             return 0;
+	for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === true) {
+            counter += 1;
+        }
+        
     }
     return counter;
 }
 
-console.log(countTrue([true, false, true, false]));
-
+console.log(countTrue([true, false, true, false, true]));
 
 
 
